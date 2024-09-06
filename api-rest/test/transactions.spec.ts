6801200f8ca858a2265241f5c1bafb08a443054e
henriqueAlgauer <algauer.henrique@gmail.com>
2024-09-05 22:57:43 -0300

    criado banco de testes

--- api-rest/test/transactions.spec.ts
+++ api-rest/test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { it, beforeAll, afterAll, describe, expect } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -10,9 +11,14 @@ describe('Transactions routes', ()=>{
     afterAll(async()=>{
         await app.close()
     })
+    
+    beforeEach(()=>{
+        execSync('npm run knex migrate:rollback --all')
+        execSync('npm run knex migrate:latest')
+    })
 
     it('should be able to create a new transaction', async ()=>{
-        const response = await request(app.server)
+        await request(app.server)
         .post('/transactions')
         .send({
             title: 'New Transaction',
@@ -21,7 +27,7 @@ describe('Transactions routes', ()=>{
         }).expect(201)
     })
 
-    it.only('should be able to list all transactions', async()=>{
+    it('should be able to list all transactions', async()=>{
         const createTransactionResponse = await request(app.server)
         .post('/transactions')
         .send({
@@ -30,11 +36,12 @@ describe('Transactions routes', ()=>{
             type: 'credit'
         })
 
-        const cookies = createTransactionResponse.get('Set-Cookie')
+        const cookies = createTransactionResponse.get('Set-Cookie') || []
 
-        const listTransactionsResponse = await request(app.server).get('/transactions').set('Cookie', cookies).expect(200)
-
-        console.log(listTransactionsResponse.body)
+        const listTransactionsResponse = await request(app.server)
+        .get('/transactions')
+        .set('Cookie', cookies)
+        .expect(200)
 
         expect(listTransactionsResponse.body.transactions).toEqual([
             expect.objectContaining({
